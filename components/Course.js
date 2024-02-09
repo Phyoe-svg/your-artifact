d8ad33fa1759067b4c1edb5a565de4b5d24c30c2
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2024-02-09 11:21:50 -0500

    [ add ] search box in courses implemented

--- components/Course.js
+++ components/Course.js
@@ -1,14 +1,25 @@
-const Course = ({ id, course_id, imgURL, title, aurthor,  info, rating, level }) => {
+const Course = ({
+  id,
+  course_id,
+  imgURL,
+  title,
+  aurthor,
+  info,
+  rating,
+  level,
+}) => {
   console.log(imgURL);
-  return `<div class="card" id="${course_id}">
-  <div class="image" id="${id}">
-      <img src="${imgURL}" alt="" srcset="">
-  </div>
-  <div class="title">${title} - <small style="color: white;">${aurthor}</small></div>
-  <div class="info">${info}</div>
-  <div class="rating">${rating}/10</div>
-  <div class="course-level">${level}</div>
-  </div>`;
+  const card = document.createElement("div");
+  card.className = "card";
+  card.id = course_id;
+  card.innerHTML = `<div class="image" id="${id}">
+  <img src="${imgURL}" alt="" srcset="">
+</div>
+<div class="title">${title} - <small style="color: white;">${aurthor}</small></div>
+<div class="info">${info}</div>
+<div class="rating">${rating}/10</div>
+<div class="course-level">${level}</div>`;
+  return card;
 };
 
 export default Course;
--- components/Datas.js
+++ components/Datas.js
@@ -1,56 +1,91 @@
 import Course from "./Course.js";
 
 const courses = [
-    {
-      id: 0,
-      course_id: 0,
-      imgURL: "../assets/icons/94a65614997aeace.jfif",
-      title: "JavaScript",
-      aurthor: "David J Martin",
-      info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum deleniti dolor vel est, iusto aliquam dolores esse odio beatae eaque voluptate eligendi. Ut aspernatur, consectetur quia inventore animi atque!",
-      rating: 0.0,
-      level: "Beginner"
-    },
-    {
-      id: 0,
-      course_id: 0,
-      imgURL: "../assets/icons/94a65614997aeace.jfif",
-      title: "Python",
-      aurthor: "David J Martin",
-      info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum deleniti dolor vel est, iusto aliquam dolores esse odio beatae eaque voluptate eligendi. Ut aspernatur, consectetur quia inventore animi atque!",
-      rating: 0.0,
-      level: "Beginner"
-    },
-    {
-      id: 0,
-      course_id: 0,
-      imgURL: "../assets/icons/94a65614997aeace.jfif",
-      title: "C",
-      aurthor: "David J Martin",
-      info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum deleniti dolor vel est, iusto aliquam dolores esse odio beatae eaque voluptate eligendi. Ut aspernatur, consectetur quia inventore animi atque!",
-      rating: 0.0,
-      level: "Beginner"
-    },
-  ];
-// add course to course section
-const display_course = document.querySelector(".display #display-courses");
-const search_box = `<div class="search">
-<input type="search" name="search" id="search" placeholder="Search">
-</div>`;
-display_course.innerHTML += search_box;
-courses.forEach( course =>{
-  display_course.innerHTML += Course(course);
-})
+  {
+    id: 0,
+    course_id: 0,
+    imgURL: "../assets/icons/94a65614997aeace.jfif",
+    title: "JavaScript",
+    aurthor: "David J Martin",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum deleniti dolor vel est, iusto aliquam dolores esse odio beatae eaque voluptate eligendi. Ut aspernatur, consectetur quia inventore animi atque!",
+    rating: 0.0,
+    level: "Beginner",
+  },
+  {
+    id: 1,
+    course_id: 0,
+    imgURL: "../assets/icons/94a65614997aeace.jfif",
+    title: "JavaScript",
+    aurthor: "David J Martin",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum deleniti dolor vel est, iusto aliquam dolores esse odio beatae eaque voluptate eligendi. Ut aspernatur, consectetur quia inventore animi atque!",
+    rating: 0.0,
+    level: "Beginner",
+  },
+  {
+    id: 2,
+    course_id: 0,
+    imgURL: "../assets/icons/94a65614997aeace.jfif",
+    title: "JavaScript",
+    aurthor: "David J Martin",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum deleniti dolor vel est, iusto aliquam dolores esse odio beatae eaque voluptate eligendi. Ut aspernatur, consectetur quia inventore animi atque!",
+    rating: 0.0,
+    level: "Beginner",
+  },
+  {
+    id: 0,
+    course_id: 0,
+    imgURL: "../assets/icons/94a65614997aeace.jfif",
+    title: "Python",
+    aurthor: "David J Martin",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum deleniti dolor vel est, iusto aliquam dolores esse odio beatae eaque voluptate eligendi. Ut aspernatur, consectetur quia inventore animi atque!",
+    rating: 0.0,
+    level: "Beginner",
+  },
+  {
+    id: 0,
+    course_id: 0,
+    imgURL: "../assets/icons/94a65614997aeace.jfif",
+    title: "C",
+    aurthor: "David J Martin",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum deleniti dolor vel est, iusto aliquam dolores esse odio beatae eaque voluptate eligendi. Ut aspernatur, consectetur quia inventore animi atque!",
+    rating: 0.0,
+    level: "Beginner",
+  },
+  {
+    id: 0,
+    course_id: 0,
+    imgURL: "../assets/icons/94a65614997aeace.jfif",
+    title: "C",
+    aurthor: "David J Martin",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur cum deleniti dolor vel est, iusto aliquam dolores esse odio beatae eaque voluptate eligendi. Ut aspernatur, consectetur quia inventore animi atque!",
+    rating: 0.0,
+    level: "Beginner",
+  },
+];
 
+// add course to course section
+const display_course = document.querySelector(
+  ".display #display-courses .courses"
+);
+
+const all_courses = document.querySelector(".all-courses");
+const searched_list = document.querySelector(".searched-list");
 const search = document.getElementById("search");
 
+courses.forEach( course => {
+  all_courses.appendChild(Course(course));
+})
+
 search.addEventListener("input", (e)=>{
-  let search_list = courses.filter( course => {
-      if(course.title.toLocaleLowerCase().startsWith(search.value)){
-        return course;
-      }
+  let matched_list = courses.filter( course =>{
+    if( course.title.toLowerCase().startsWith(search.value)){
+      return course;
+    }
   })
-  search_list.forEach( search_item => {
-    display_course.innerHTML = Course(search_item);
+  while( all_courses.hasChildNodes() ){
+    all_courses.removeChild(all_courses.firstChild);
+  }
+  matched_list.forEach( course =>{
+    all_courses.appendChild(Course(course));
   })
 })
