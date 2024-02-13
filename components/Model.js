28ee0d7ae0f4f9efe5182e663b0afb1e38090481
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2024-02-13 09:33:30 -0500

    [ add ] Model with tested like db

--- components/Model.js
+++ components/Model.js
@@ -1,81 +1,159 @@
-// check courses localStorage is already exists or created new
-if (localStorage.getItem("courses") === null || localStorage.getItem("users") === null ) {
-  localStorage.setItem("courses", "[]");
-  localStorage.setItem("users", "[]");
-}
-// convert string into arry from localStorage "courses"
-let current_courses = JSON.parse(localStorage.getItem("courses"));
-let current_users = JSON.parse(localStorage.getItem("users"));
-
-
-const Model = {
-  User: {
-    // create new User { parem: userdata is an object parameter }
-    insert: ( user_data )=>{
-      // check user is already exit or not
-      // console.log(user_data);
-      const user_exists = current_users.filter( user => user.email === user_data.email);
-      // check user is exists or not by checking the result array filter list length 
-      if( user_exists.length === 0 ){
-        // push to current user array
-        current_users.push(user_data);
-        // update to localStorage: users
-        localStorage.setItem("users", JSON.stringify(current_users));
-        return "User created", true
-      }
-      return "Aleady Exists", false
-    },
-
-    delete: (id)=>{
-      const will_deleted_user = current_users.findIndex( user => user.id === id);
-      if(current_users[will_deleted_user] === undefined ){
-        return "User not exist", false
-      }
-      // remove from the array temp
-      current_users.splice(will_deleted_user, 1);
-      // write to localStorage 
-      localStorage.setItem("users", JSON.stringify(current_users));
-      return "Deleted", true
-    }
-  },
-  // Course object for managing course data
-  Courses: {
-    // insert data into courses key in localStorage
-    // parem: * object as data parameter *
-    insert: (new_course) => {
-      //  push new course into courses
-      current_courses.push(new_course);
-      localStorage.setItem("courses", JSON.stringify(current_courses));
-    },
-
-    // parem: * first take id for find the index of data and second update content will update it *
-    update: ({ id, update_content }) => {
-      // check where is this content index is
-      let course_for_update = current_courses.findIndex((course) => {
-        return course.id === id;
-      });
-      // change the content where is the course arry in
-      current_courses[course_for_update] = update_content;
-      // update to localStorage
-      localStorage.setItem("courses", JSON.stringify(current_courses));
-    },
-    // this will take one parameter id of course
-    delete: (id) => {
-      // find course
-      if (current_courses[id] === undefined) {
-        return "Wrong course ID or Empty data";
-      }
-      // find index of will deleted data
-      let will_deleted = current_courses.findIndex(
-        (course) => course.id === id
-      );
-      // delete from arry
-      current_courses.splice(will_deleted, 1);
-      // update to localStorage
-      localStorage.setItem("courses", JSON.stringify(current_courses));
-      return "Deleted";
+const sample_form = {
+  test_db: {
+    // user table
+    Users: {
+      //   0: {
+      //     id: 0,
+      //     username: "domak",
+      //     email: "domak@gmail.com",
+      //     password: "dompass",
+      //     since: "13 Feb 2024",
+      //     profile: "./profile.jpg",
+      //     bio: "take the risk, aganist the world",
+      //   },
+      //   1: {
+      //     id: 1,
+      //   username: "Kyaw Than",
+      //   email: "domak1090@gmail.com",
+      //   password: "dompass",
+      //   since: "13 Feb 2024",
+      //   profile: "./profile.jpg",
+      //   bio: "take the risk, aganist the world",
+      //   },
     },
   },
 };
+// let obj_length = Object.keys(sample_form.test_db.Users).length;
+// console.log(obj_length)
+// console.log(sample_form.test_db.Users[obj_length]);
 
-export default Model;
+class Model {
+  db;
+  db_name = "learning";
+  table_name = this.constructor.name;
+  constructor() {
+    if (localStorage.getItem(this.db_name) === null) {
+      localStorage.setItem(this.db_name, "{}");
+    }
+    this.db = JSON.parse(localStorage.getItem(this.db_name));
+  }
+
+  // data insertion
+  insert(datas) {
+    // check table is exist or create new
+    if (this.db[this.table_name] === undefined) {
+      this.db[this.table_name] = {};
+    }
+    // get the table object
+    let table = this.db[this.table_name];
+    // generate new id key
+    let id = Object.keys(table).length;
+    // unique id to incoming new data insertion
+    datas.id = id;
+    // if the table is user, then check user is already register or not
+    if (this.table_name === "User") {
+      const users = Object.values(table);
+      const user = users.filter((user) => user.email === datas.email);
+      if (user.length != 0) {
+        return "User Already Exists";
+      }
+    }
+    table[id] = datas;
+    this.db[this.table_name] = table;
+    return "Added";
+  }
+
+  //   update data with unique id
+  update(update_data) {
+    if (update_data.id === undefined) {
+      return "data doesn't exists";
+    }
+    let table = this.db[this.table_name];
+    if (table === undefined) {
+      return "Table haven't created yet";
+    }
+    table[update_data.id] = update_data;
+    return "Tempo updated";
+  }
+  //   deleting the data with unique id
+  remove(id) {
+    let table = this.db[this.table_name];
+    // delete the target data from table
+    delete table[id];
+    // overwrite the table and return true
+    this.db[this.table_name] = table;
+    return true;
+  }
+
+  //  commit to actual data collection localStorge;
+  commit() {
+    localStorage.setItem(this.db_name, JSON.stringify(this.db));
+    return true;
+  }
+
+  //   get all datas
+  getAll() {
+    return JSON.parse(localStorage.getItem(this.db_name))[this.table_name];
+  }
+}
+
+class User extends Model {}
+class Course extends Model {}
+
+const course = new Course();
+console.log(
+  course.update({
+    id: 0,
+    aurthor: "Martin2020",
+    publish_date: "13 feb 24",
+    duration: "10 hours",
+    logo: "./logo.png",
+    level: "Beginner",
+    desc: "Amazing course",
+  })
+);
+course.commit();
+// localStorage.clear();
+// const user = new User();
+// console.log(
+//   user.insert({
+//     username: "Kyaw Than",
+//     email: "domak1090@gmail.com",
+//     password: "dompass",
+//     since: "13 Feb 2024",
+//     profile: "./profile.jpg",
+//     bio: "take the risk, aganist the world",
+//   })
+// );
+// console.log(
+//   user.insert({
+//     username: "Kyaw Than",
+//     email: "domak1010@gmail.com",
+//     password: "dompass",
+//     since: "13 Feb 2024",
+//     profile: "./profile.jpg",
+//     bio: "take the risk, aganist the world",
+//   })
+// );
+// console.log(
+//   user.update({
+//     id: 0,
+//     username: "May",
+//     email: "May@gmail.com",
+//     password: "dompass",
+//     since: "13 Feb 2024",
+//     profile: "./profile.jpg",
+//     bio: "take the risk, aganist the world",
+//   })
+// );
+// user.commit();
+
+// console.log(user.getAll());
+// user.commit();
+// const datas = JSON.parse(localStorage.getItem("learning"));
+// console.log(datas.User);
+
+// const user = new User();
+// user.remove(0);
+// user.commit();
+// console.log(user.getAll());
